29b3f6bbf3e4f2bce6c3ec745df803a1bf99e833
Nick <ptnguyen1901@gmail.com>
2020-08-11 17:00:27 -0400

    Day 2

--- Day1.py
+++ Day1.py
@@ -71,6 +71,22 @@ class Graph:
                 for next_vert in self.get_neighbors(v):
                     s.push(next_vert)
 
+        def bfs(self, starting_vertex_id, target_vertex_id):
+            # Create an empty queue and enqueue A PATH TO the starting vertex ID
+            # Create a Set to store visited vertices
+            # While the queue is not empty...
+                # Dequeue the first PATH
+                # Grab the last vertex from the PATH
+                # If that vertex has not been visited...
+                    # CHECK IF IT'S THE TARGET
+                        # IF SO, RETURN PATH
+                    # Mark it as visited...
+                    # Then add A PATH TO its neighbors to the back of the queue
+                        # COPY THE PATH
+                        # APPEND THE NEIGHBOR TO THE BACK
+
+
+
 g = Graph()
 
 g.add_vertex('A')
---
+++ Day2.py
@@ -0,0 +1,25 @@
+# Word Ladders
+# -------------------
+
+# Given two words (begin_word and end_word), and a dictionary's word list, return the shortest transformation sequence
+# from begin_word to end_word, such that:
+
+# Only one letter can be changed at a time.
+
+# Each transformed word must exist in the word list. Note that begin_word is not a transformed word.
+
+# Note:
+
+
+# Return None if there is no such transformation sequence.
+# All words contain only lowercase alphabetic characters.
+# You may assume no duplicates in the word list.
+# You may assume begin_word and end_word are non-empty and are not the same.
+
+
+# start: hit
+# ending: cog
+
+# hit -> hot -> hog -> cog
+# hit -> hot -> cot -> cog
+
